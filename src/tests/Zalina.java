22cf118b95c61949b84c72b416cd53d5a8129d31
Zalina Buruzheva <zalinabu555@gmail.com>
2021-02-03 12:45:08 -0500

    1/2 first part

--- src/tests/Zalina.java
+++ src/tests/Zalina.java
@@ -1,2 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Zalina {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\VZD\\Desktop\\SELENIUM\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://orangehrm-demo-6x.orangehrmlive.com/client/#/dashboard");
+        driver.findElement(By.id("btnLogin")).click();
+    }
 }
